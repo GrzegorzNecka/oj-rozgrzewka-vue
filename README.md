d011602f88bbcae7bbc04b76d8c4f457910480a5
GrzegorzNecka <strzypior@gmail.com>
2021-02-10 15:15:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oj-rozgrzewka-vue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/oj-rozgrzewka-vue)
---
+++ index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+
+Vue.component("text-editor", {
+  data: function() {
+    return {
+      message: "✏️ Start writing..."
+    };
+  },
+  methods: {
+    viewMessage: function() {
+      alert(this.message);
+    }
+  },
+
+  template: `
+  <div class="block">
+    <textarea 
+      name="editor"
+      v-model="message"
+      class="p-5 h-40 w-64 shadow-md rounded-md">
+    </textarea>
+    <div class="text-right">
+      <button  v-on:click="viewMessage" class="block bg-blue-500 font-bold py-2 px-4 rounded text-white button float-right">
+          Save! 
+      </button>
+    </div>
+  </div>
+`
+});
+
+new Vue({
+  el: "#app",
+  data: {}
+});
